9d0bd60a2fbb7906c30a30ba9868734b17ea100e
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-10-30 03:53:42 +0200

    Create Network.predict and Network.predict_layer

--- src/network.py
+++ src/network.py
@@ -11,12 +11,36 @@ class Network:
         self.theta = theta
         self.epochs = epochs
         self.minj = minj
-        layers = np.ndarray(dtype=Layer, shape=(nlayers))
+        self.layers = np.ndarray(dtype=Layer, shape=(nlayers))
         type = 'i'
         for i in range(nlayers):
             n_prev = nodes[i - 1] if i > 0 else 1
-            layers[i] = Layer(i, nodes[i], n_prev, type, beta, theta, f)
+            self.layers[i] = Layer(i, nodes[i], n_prev, type, beta, theta, f)
             type = 'h' if i < nlayers - 2 else 'o'
+
+
+    def predict(self, x):
+        y = x
+        for i in range(self.nlayers):
+            print(y)
+            y = self.predict_layer(y, i)
+        return y
+
+
+    def predict_layer(self, x, i):
+        n_i = self.nodes_per_layer[i]
+        y = np.ndarray(shape=(n_i))
+        if i == 0: # input layer only `sees` one entry of the input.
+            for j in range(n_i):
+                node_i = self.layers[i].nodes[j]
+                node_i.get_y(x[j])
+                y[j] = node_i.y
+        else:
+            for j in range(n_i): # rest of the layers are fully connected.
+                node_i = self.layers[i].nodes[j]
+                node_i.get_y(x)
+                y[j] = node_i.y
+        return y
 
 
     nlayers:int                # number of layers
