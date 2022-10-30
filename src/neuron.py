968974082942d4a5920c679121994ea373601ec8
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-10-30 03:44:54 +0300

    Make all attributes public for Neuron, remove redundant concstructor

--- src/neuron.py
+++ src/neuron.py
@@ -1,37 +1,38 @@
 import numpy as np
 
+
 class Neuron:
-    def __init__(self, id, lid, w, type, n_in, n_out, f):
-        self._id = id
-        self._lid = lid
-        self._w = w
-        self._type = type
-        self._n_in = n_in
-        self._n_out = n_out
+    def __init__(self, id, lid, w, type, beta, theta, f):
+        nw = len(w)
+        self.id = id
+        self.lid = lid
+        self.w = w
+        self.type = type
         self.f = f
-        self.y = np.ndarray
-        self._x = np.ndarray
-        self._w = np.ndarray
+        self.beta = beta
+        self.n_in = nw - 1 if nw > 0 else 0
+        self.x = np.ndarray(dtype=np.float16, shape=(nw))
+        self.x[0] = theta
 
 
     def __str__(self):
-        txt = "node[{}(L{})] ({})"
-        return txt.format(self._id, self._lid, self._type)
+        txt = "node[L{},{}] ({}) : {}"
+        return txt.format(self.lid, self.id, self.type, self.w)
 
 
     def get_y(self):
-        self._u = np.dot(self._x, self._w)
-        self.y = self.f(self._u)
+        self.u = np.dot(self.x, self.w)
+        self.y = self.f(self.u)
 
 
-    y:np.ndarray
-    f:callable
-    _x:np.ndarray
-    _w:np.ndarray
-    _u:np.ndarray
-    _d:float
-    _type:str
-    _n_in:int
-    _n_out:int
-    _id:int
-    _lid:int
+    y:np.float32    # output
+    f:callable      # activation function
+    beta:np.float32 # learning rate
+    x:np.ndarray    # input
+    w:np.ndarray    # weights
+    u:np.ndarray    # f(x)
+    d:float         # targets
+    type:str        # [input, output, or hidden]
+    n_in:int        # len(x)
+    id:int          # id in the layer
+    lid:int         # id of the layer
