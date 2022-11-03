43a877c4703743a464a8220a1d115543a25cdfb3
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-03 23:28:29 +0200

    Move the initialization of a Neuron in a separate routine for Layer

--- src/layer.py
+++ src/layer.py
@@ -2,29 +2,35 @@ from neuron import *
 
 
 class Layer:
-    def __init__(self, lid, n, n_prev, type, beta, theta, f):
+    def __init__(self, lid, n, n_prev, type, eta, theta, f):
         check_for_type(type)
         scal = np.sqrt(2 / n_prev) # factor by which the random weights will be multiplied.
         self.n = n
         self.f = f
         self.lid = lid
         self.type = type
-        self.beta = beta
+        self.eta = eta
         self.nodes = np.ndarray(dtype=Neuron, shape=(n))
         for i in range(n):
-            w = np.random.rand(n_prev + 1) * scal
-            w[0] = -1
-            self.nodes[i] = Neuron(i, lid, w, type, beta, theta, f) # `He` weight initialization.
-            print(self.nodes[i])
+            self.nodes[i] = initialize_node(i, lid, eta, theta, n_prev, scal, f)
 
 
     f:callable       # activation function
-    beta:np.float32  # learning rate
+    eta:np.float32   # learning rate
     lid:int          # layer id
     n:int            # number of neurons
     type:str         # [input, output, or hidden]
     nodes:np.ndarray # list of neurons in layer
     e:np.float32     # sum of errors
+
+
+"""
+Initialize a neuron using He random weight assignment.
+"""
+def initialize_node(i, lid, eta, theta, n_prev, scal, f):
+    w = np.random.rand(n_prev + 1) * scal
+    w[0] = -1
+    return Neuron(i, lid, w, type, eta, theta, f)
 
 
 def check_for_type(type):
