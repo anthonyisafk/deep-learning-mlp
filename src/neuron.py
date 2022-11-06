9ef0cd343cc7d46e260fd02c6f1d61d41969cf05
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-06 09:15:50 +0200

    Add `delta` to Neuron, add setters for `w`, `d`, `e`

--- src/neuron.py
+++ src/neuron.py
@@ -2,27 +2,29 @@ import numpy as np
 
 
 class Neuron:
-    y:np.float32     # output
-    f:callable       # activation function
-    df:callable      # activation function derivative
-    eta:np.float32   # learning rate
-    w:np.ndarray     # weights
-    u:np.ndarray     # f(x)
-    d:np.float32     # target
-    t:str            # [input, output, or hidden]
-    n_in:int         # len(x)
-    id:int           # id in the layer
-    lid:int          # id of the layer
-    e:np.float32     # error signal
+    y:np.float32      # output
+    f:callable        # activation function
+    df:callable       # activation function derivative
+    eta:np.float32    # learning rate
+    _w:np.ndarray     # weights
+    u:np.ndarray      # f(x)
+    _d:np.float32     # target
+    t:str             # [input, output, or hidden]
+    n_in:int          # len(x)
+    id:int            # id in the layer
+    lid:int           # id of the layer
+    _e:np.float32     # error value
+    _delta:np.float32 # back propagation error signal
 
 
     def __init__(self, id, lid, w, t, eta, f, df):
         nw = len(w)
         self.u = 0
         self.y = 0
+        self._delta = 0
         self.id = id
         self.lid = lid
-        self.w = w
+        self._w = w
         self.t = t
         self.f = f
         self.df = df
@@ -36,7 +38,7 @@ class Neuron:
 
 
     def get_u(self, x:np.ndarray):
-        return np.dot(np.concatenate(([-1], x), axis=None), self.w)
+        return np.dot(x, self.w)
 
 
     def get_y(self):
@@ -45,3 +47,36 @@ class Neuron:
 
     def get_error(self):
         return self.d - self.y;
+
+    # Properties that need to be turned `private`, to create setters. #
+
+    @property
+    def w(self):
+        return self._w
+    @w.setter
+    def w(self, w):
+        self._w = w
+
+
+    @property
+    def d(self):
+        return self._d
+    @d.setter
+    def d(self, d):
+        self._d = d
+
+
+    @property
+    def e(self):
+        return self._e
+    @e.setter
+    def e(self, e):
+        self._e = e
+
+
+    @property
+    def delta(self):
+        return self._delta
+    @delta.setter
+    def delta(self, delta):
+        self._delta = delta
