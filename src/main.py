de5e6c5cf17b45292c9042c048bb419e56a896ab
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-07 01:26:34 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,9 +9,32 @@ Aristotle University Thessaloniki - School of Informatics.
 """
 
 from network import *
+from activation import *
+from scipy.io import loadmat
+from matplotlib import pyplot
 
-def f(u):
-    return 1 if u > 0 else -1
+nlayers = 4
+nodes_per_layer = np.array([3, 4, 4, 3])
+eta = 0.10
+theta = 0.0
+activf = activation.logistic
+batch_size = 1
+epochs = 10
+
 
 if __name__ == "__main__":
-    dnn = Network(4, np.array([3, 4, 4, 3]), 0.05, 0.1, 10, 20, f)
+    mnist = loadmat("mnist/mnist-original.mat")
+    data = mnist['data'].T
+    pyplot.plot(data[0])
+    pyplot.show()
+
+
+    # mlp = Network(nlayers, nodes_per_layer, eta, theta, activf)
+
+    # x = [[1, 2, 3], [2, 3, 4], [2, 3, 4.5]]
+    # y = [[1, 0, 0], [0, 0, 1], [0, 0, 1]]
+
+    # x = np.concatenate((x, x, x))
+    # y = np.concatenate((y, y, y))
+
+    # mlp.train(x, y, batch_size, epochs, 2.1)
