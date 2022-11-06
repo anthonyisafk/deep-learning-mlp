db3f59c743aad6946ff9f38847caffe8e8b69fb4
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-06 09:15:01 +0200

    Add calculation of delta and weight update to utils.py

--- src/utils.py
+++ src/utils.py
@@ -21,13 +21,27 @@ def check_for_type(t):
 
 # ---------- NETWORK TRAINING UTILITIES ---------- #
 
-def training_predict_first_layer(node, x):
+def training_predict_node(node, x):
     u = node.get_u(x)
     y = node.get_y()
     return u, y
 
 
-def training_predict_hidden_layer(node, x):
-    u = node.get_u(x)
-    y = node.get_y()
-    return u, y
+def set_targets(out_layer, nout, d):
+    for n in range(nout):
+        out_layer.nodes[n].d = d[n]
+
+
+def update_output_weights(last_hidden, out_layer, nout, eta):
+    for i in range(nout):
+        node_i = out_layer.nodes[i]
+        node_i.w[0] -= eta * node_i.delta
+        for j in range(node_i.n_in):
+            node_i.w[j + 1] += eta * node_i.delta * last_hidden.nodes[j].y
+
+
+def calculate_delta(dfu, next_layer, i):
+    delta = 0.0
+    for j in range(next_layer.n):
+        delta += dfu * next_layer.nodes[j].delta * next_layer.nodes[j].w[i]
+    return delta
