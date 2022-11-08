5c63319f6864be5e65f22c45076637d58b28eb82
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-08 04:31:29 +0200

    Use momemtum for update_output_weights in utils.py

--- src/utils.py
+++ src/utils.py
@@ -22,9 +22,9 @@ def check_for_type(t):
 # ---------- NETWORK TRAINING UTILITIES ---------- #
 
 def training_predict_node(node, x):
-    u = node.get_u(x)
+    node.u = node.get_u(x)
     y = node.get_y()
-    return u, y
+    return node.u, y
 
 
 def set_targets(out_layer, nout, d):
@@ -32,16 +32,20 @@ def set_targets(out_layer, nout, d):
         out_layer.nodes[n].d = d[n]
 
 
-def update_output_weights(last_hidden, out_layer, nout, eta):
+def update_output_weights(last_hidden, out_layer, nout, eta, alpha):
     for i in range(nout):
         node_i = out_layer.nodes[i]
-        node_i.w[0] -= eta * node_i.delta
+        wprev = node_i.wprev[0]
+        node_i.wprev[0] = node_i.w[0]
+        node_i.w[0] -= eta * node_i.delta + alpha * wprev
         for j in range(node_i.n_in):
-            node_i.w[j + 1] += eta * node_i.delta * last_hidden.nodes[j].y
+            wprev = node_i.wprev[j + 1]
+            node_i.wprev[j + 1] = node_i.w[j + 1]
+            node_i.w[j + 1] += eta * node_i.delta * last_hidden.nodes[j].y + alpha * wprev
 
 
 def calculate_delta(dfu, next_layer, i):
     delta = 0.0
     for j in range(next_layer.n):
-        delta += dfu * next_layer.nodes[j].delta * next_layer.nodes[j].w[i]
-    return delta
+        delta += next_layer.nodes[j].delta * next_layer.nodes[j].w[i + 1]
+    return dfu * delta
