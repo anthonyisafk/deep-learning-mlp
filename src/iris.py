f11b6593e2e991f52e64e2ed88607a298535cec5
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-09 23:19:36 +0200

    Organize iris.py into routines

--- src/iris.py
+++ src/iris.py
@@ -5,9 +5,9 @@ from network import *
 
 train_fraction = 0.7
 minJ = 0.05
-alpha = 2.6e-6
+alpha = 2e-6
 theta = 0.0
-eta = 0.029
+eta = 0.025
 epochs = 100
 batch_size = 1
 f = activation.tanh
@@ -18,7 +18,7 @@ species = {
     'Iris-virginica': 2,
 }
 
-if __name__ == "__main__":
+def main():
     df = pd.read_csv('iris/iris.csv', header=0)
     nrows = len(df.values[:, 0])
     ncols = len(df.values[0, :])
@@ -27,14 +27,29 @@ if __name__ == "__main__":
     for i in range(nrows):
         y[i, species[df.values[i, ncols-1]]] = 1.0
 
+    samples, targets = split_into_classes(x, y)
+    x_train, x_test, y_train, y_test = split_trainset_testset(samples, targets)
+
+    nin = len(x[0])
+    nout = len(y[0])
+    nodes = [nin, 7, nout]
+    mlp = Network(nodes, eta, theta, alpha, f)
+    mlp.train(x_train, y_train, batch_size, epochs, minJ)
+
+    test_rate = test_network(x_test, y_test, mlp, True)
+
+
+def split_into_classes(x, y):
     samples = [[] for _ in range(3)]
     targets = [[] for _ in range(3)]
     for v in species.values():
         indices = np.asarray(y[:, v] == 1.0)
         samples[v] = x[indices]
         targets[v] = y[indices]
+    return samples, targets
 
 
+def split_trainset_testset(samples, targets):
     x_train = []
     x_test = []
     y_train = []
@@ -42,7 +57,6 @@ if __name__ == "__main__":
     for v in species.values():
         nv = len(samples[v])
         ntrain = int(train_fraction * nv)
-
         if v == 0:
             x_train = samples[v][0:ntrain]
             x_test = samples[v][ntrain:]
@@ -53,25 +67,13 @@ if __name__ == "__main__":
             x_test = np.concatenate((x_test, samples[v][ntrain:]))
             y_train = np.concatenate((y_train, targets[v][0:ntrain]))
             y_test = np.concatenate((y_test, targets[v][ntrain:]))
+    return x_train, x_test, y_train, y_test
 
-    nin = len(x[0])
-    nout = len(y[0])
-    nodes = [nin, 7, nout]
-    mlp = Network(len(nodes), nodes, eta, theta, alpha, f)
 
-    # for l in mlp.layers:
-    #     for i in range(l.n):
-    #         print(l.nodes[i])
-
-    mlp.train(x_train, y_train, batch_size, epochs, minJ)
-
-    # for l in mlp.layers:
-    #     for i in range(l.n):
-    #         print(l.nodes[i])
-
+def test_network(x, y, mlp, print_result=True):
     success = 0
-    ntests = len(y_test)
-    for tx, ty in zip(x_test, y_test):
+    ntests = len(y)
+    for tx, ty in zip(x, y):
         res = mlp.training_predict(tx)
         # print(res)
         pred = np.argmax(res)
@@ -79,9 +81,12 @@ if __name__ == "__main__":
         print(f" -- | {res} | prediction : {pred}, actual y : {target}")
         if pred == target:
             success += 1
-    print(f"\n  >>> Passed {success} / {ntests} tests.")
+
+    if print_result:
+        print(f"\n  >>> Passed {success} / {ntests} tests.")
+    return success / ntests
 
 
 
-
-
+if __name__ == "__main__":
+    main()
