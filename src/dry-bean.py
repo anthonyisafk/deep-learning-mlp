12b9af2e5ab2a698d9e31ec0103e15039a495a7c
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-11-27 11:27:42 +0200

    Update dry-bean.py, wine.py (done with them for now)

--- src/dry-bean.py
+++ src/dry-bean.py
@@ -18,12 +18,12 @@ from utils.testing import *
 
 train_fraction = 0.6
 minJ = 0.05
-alpha = 3e-5
-theta = 0.00
-eta = 0.20
+alpha = 0.0001
+theta = 0.0
+eta = 0.001
 epochs = 120
 batch_size = 1
-f = activation.logistic
+f = [activation.logistic, activation.logistic, activation.relu]
 
 species = {
     'SEKER': 0,
@@ -52,6 +52,7 @@ def main():
     y = np.zeros(shape=(nrows,nlabels))
     for i in range(nrows):
         y[i, int(d[i])] = 1.0
+    x = preprocessing.normalize(x)
 
     samples, targets = split_into_classes(x, y, nlabels, species)
     # frequencies = np.zeros(shape=(nlabels))
@@ -62,16 +63,12 @@ def main():
     # for i in range(nlabels):
     #     targets[i] *= scales[i]
     x_train, x_test, y_train, y_test = split_trainset_testset(samples, targets, train_fraction, species)
-    x_train = preprocessing.normalize(x_train)
-
-
-    # x_test = preprocessing.normalize(x_test)
 
     nin = len(x[0])
     nout = len(y[0])
     nhidden = int(2 * (nin + nout) / 3)
     nhidden_layer = int(nhidden / 2)
-    nodes = [nin, nhidden, nout]
+    nodes = [nin, nin + nout, nin + nout, nout]
     mlp = Network(nodes, eta, theta, alpha, f)
     mlp.train(x_train, y_train, batch_size, epochs, minJ)
 
--- src/wine.py
+++ src/wine.py
@@ -13,16 +13,19 @@ Aristotle University Thessaloniki - School of Informatics.
 import pandas as pd
 from classes.network import *
 from utils.testing import *
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import normalize
 
 
 train_fraction = 0.6
-minJ = 0.05
-alpha = 4e-4
+# minJ = 0.05
+minJ = 0.10 
+alpha = 0.0001
 theta = 0.0
-eta = 0.08
+eta = 0.001
 epochs = 100
 batch_size = 1
-f = activation.logistic
+f = [activation.logistic, activation.logistic, activation.logistic]
 
 labels = {
     3: 0,
@@ -41,32 +44,45 @@ def main():
     ncols = len(df.values[0, :])
     nlabels = len(labels)
 
-    x = df.values[:, 0:ncols - 1]
-    y = np.zeros(shape=(nrows,nlabels))
-    for i in range(nrows):
-        y[i, labels[df.values[i, ncols-1]]] = 1.0
+    # x = df.values[:, 0:ncols - 1]
+    # d = np.zeros(shape=(nrows))
+    # for i in range(nrows):
+    #     d[i] = int(labels[df.values[i, ncols-1]])
 
-    samples, targets = split_into_classes(x, y, nlabels, labels)
-    # frequencies = np.zeros(shape=(nlabels))
-    # for i in range(nlabels):
-    #     frequencies[i] = len(targets[i])
-    # max_freq = np.max(frequencies)
-    # ncopies = [int(np.ceil(max_freq / f)) - 1 for f in frequencies]
-    # for i in range(nlabels):
-    #     scopy = samples[i]
-    #     tcopy = targets[i]
-    #     for _ in range(ncopies[i]):
-    #         samples[i] = np.concatenate((samples[i], scopy))
-    #         targets[i] = np.concatenate((targets[i], tcopy))
-    x_train, x_test, y_train, y_test = split_trainset_testset(samples, targets, train_fraction, labels)
+    # sm = SMOTE(k_neighbors=2)
+    # x, d = sm.fit_resample(x, d)
+    # nrows = len(x)
+    # y = np.zeros(shape=(nrows, nlabels))
+    # for i in range(nrows):
+    #     y[i, int(d[i])] = 1.0
+    # x = normalize(x, norm="l2")
 
+    n = 100
+    x = np.zeros(shape=(2*n,10))
+    y = np.zeros(shape=(2*n,2))
+    nlabels = 2
+    for i in range(n):
+        y[i, 0] = 1.0
+    for i in range(n,2*n):
+        y[i, 1] = 1.0
+    for i in range(n):
+        x[i, :] = np.random.normal(loc=-2.0, scale=0.5)
+    for i in range(n,2*n):
+        x[i, :] = np.random.normal(loc=-0.5, scale=0.5)
 
+    perm = np.random.permutation(2*n)
+    x, y = x[perm], y[perm]
+
+    # samples, targets = split_into_classes(x, y, nlabels, labels)
+    # x_train, x_test, y_train, y_test = split_trainset_testset(samples, targets, train_fraction, labels)
+    samples, targets = split_into_classes(x, y, nlabels, None)
+    x_train, x_test, y_train, y_test = split_trainset_testset(samples, targets, train_fraction, None)
 
     nin = len(x[0])
     nout = len(y[0])
     total_hidden = 2 * nin / 3 + nout
-    per_hidden_layer = int(total_hidden / 2)
-    nodes = [nin, per_hidden_layer, per_hidden_layer, nout]
+    # per_hidden_layer = int(total_hidden / 2)
+    nodes = [nin, nin + nout, nin + nout, nout]
     mlp = Network(nodes, eta, theta, alpha, f)
     mlp.train(x_train, y_train, batch_size, epochs, minJ)
 
